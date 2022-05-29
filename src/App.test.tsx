8d4cbeb62e2dde1988a98040fec745270d916975
Bruno Felaco <4786734+bfelaco@users.noreply.github.com>
2022-05-29 10:01:52 -0400

    Fix unit-test description.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Wordless link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Wordless/i);
   expect(linkElement).toBeInTheDocument();
